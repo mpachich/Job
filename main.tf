f8db6e6a2fe415edfb1ce883352b67b56f674068
Manan <mpachich@in.ibm.com>
2018-09-06 14:45:16 +0530

    3

--- main.tf
+++ main.tf
@@ -33,18 +33,21 @@ resource "kubernetes_pod" "jenkins" {
   }
 }
 
-provider "jenkins" {
-  server_url = "http://localhost:8080/"
-  username   = "user1"
-  password   = "user1"
-}
+# provider "jenkins" {
+#   server_url = "http://localhost:8080/"
+#   username   = "user1"
+#   password   = "user1"
+# }
 
-resource "jenkins_job" "first" {
-  name         = "TerrTest"
-  display_name = "First terraform test"
-  description  = "This makes a project using terraform"
-  disabled     = false
 
-  parameters = {}
-  template   = "file://./job_config.xml"
-}
+# resource "jenkins_job" "first" {
+#   name         = "TerrTest"
+#   display_name = "First terraform test"
+#   description  = "This makes a project using terraform"
+#   disabled     = false
+
+
+#   parameters = {}
+#   template   = "file://./job_config.xml"
+# }
+

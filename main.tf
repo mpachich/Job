76a874c99883cc9c3fc7bf96c94557e5d48798f9
Manan <mpachich@in.ibm.com>
2018-09-19 11:47:25 +0530

    1

--- main.tf
+++ main.tf
@@ -43,6 +43,25 @@ provider "kubernetes" {
   # token=""
 }
 
+resource "kubernetes_service" "test_service" {
+  metadata {
+    name = "service-example"
+  }
+
+  spec {
+    selector {
+      app = "${kubernetes_pod.jenkins.metadata.0.labels.app}"
+    }
+
+    port {
+      port        = 8080
+      target_port = 8080
+    }
+
+    type = "NodePort"
+  }
+}
+
 resource "kubernetes_pod" "jenkins" {
   metadata {
     name = "jenkins-example"
